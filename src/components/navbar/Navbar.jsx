a6c674cb2e35ef9e60d46f004977531e04aac6af
agarwal1999 <agarwal18b@iitg.ac.in>
2021-07-26 18:29:37 +0530

    changed product name

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,10 +16,7 @@ const Navbar = () => {
     <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
         <div className="left">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
-            alt=""
-          />
+          <h2>BingeHub</h2>
           <span>HomePage</span>
           <span>Series</span>
           <span>Movies</span>
